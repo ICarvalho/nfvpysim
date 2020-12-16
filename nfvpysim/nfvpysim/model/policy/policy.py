42ba4fd38fab2cea07dc4dc769cda207d309436f
igor <igfuca@gmail.com>
2020-12-16 19:51:57 -0300

    new updates

--- nfvpysim/nfvpysim/model/policy/policy.py
+++ nfvpysim/nfvpysim/model/policy/policy.py
@@ -69,27 +69,29 @@ class GreedyWithOnlinePlacementPolicy(Policy):
         path = self.view.shortest_path(ingress_node, egress_node)
         self.controller.start_session(time, ingress_node, egress_node, sfc)
         missed_vnfs = []
+        vnf_status = {vnf: False for vnf in sfc}
         for u, v in path_links(path):
             self.controller.forward_request_hop(u, v)
-            for vnf in sfc:
-                vnf_status = {vnf: False for vnf in sfc}
-                if self.controller.get_vnf(v, vnf) and vnf_status[vnf] is False: # vnf on node and processed
-                    self.controller.vnf_proc(vnf)
-                    vnf_status[vnf] = True
-                    continue
-                elif self.controller.get_vnf(v, vnf) and vnf_status[vnf] is True: # vnf has already been processed in previous node
-                    continue
-                elif not self.controller.get_vnf(v, vnf): # vnf not on node and not processed yet
-                    missed_vnfs.append(vnf)
-                    continue
+            if self.model.is_nfv_node(v):
+                for vnf in sfc:
+                    if self.controller.get_vnf(v, vnf) and vnf_status[vnf] is False: # vnf on node and processed
+                        self.controller.vnf_proc(vnf)
+                        vnf_status[vnf] = True
 
-                if len(missed_vnfs) >= 1 and any(value is False for value in vnf_status.values()):
-                    target_nfv_node = self.controller.find_target_nfv_node(path, missed_vnfs)
-                    closest_nfv_node = self.controller.get_closest_nfv_node(path)
-                    if v == closest_nfv_node and v == target_nfv_node:
-                        for missed_vnf in missed_vnfs:
-                            self.model.add_vnf(missed_vnf)
-                            vnf_status = {missed_vnf: True for missed_vnf in missed_vnfs}
+                    elif self.controller.get_vnf(v, vnf) and vnf_status[vnf] is True: # vnf has already been processed in previous node
+                        continue
+
+                    elif not self.controller.get_vnf(v, vnf) and vnf_status[vnf] == False: # vnf not on node and not processed yet
+                        missed_vnfs.append(vnf)
+                        continue
+
+            if len(missed_vnfs) >= 1 and any(value is False for value in vnf_status.values()):
+                target_nfv_node = self.controller.find_target_nfv_node(path, missed_vnfs)
+                closest_nfv_node = self.controller.get_closest_nfv_node(path)
+                if v == closest_nfv_node and v == target_nfv_node:
+                    for missed_vnf in missed_vnfs:
+                        self.model.put_vnf(missed_vnf)
+                        vnf_status[missed_vnf] = True
                 if all(value is True for value in vnf_status.values() and v == egress_node):
                     break
             if self.collector is not None and self.session['log']:
