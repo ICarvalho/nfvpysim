bf19043138047c72cf371db2ac5c893eed139753
igor <igfuca@gmail.com>
2021-02-23 18:50:20 -0300

    3e

--- nfvpysim/nfvpysim/execution/engine.py
+++ nfvpysim/nfvpysim/execution/engine.py
@@ -34,19 +34,38 @@ def exec_experiment(topology, workload, netconf, policy, nfv_cache_policy, colle
     results : Tree
         A tree with the aggregated simulation results from all collectors
     """
-    model = NetworkModel(topology, nfv_cache_policy, **netconf)
-    view = NetworkView(model)
-    controller = NetworkController(model)
+    model_baseline = NetworkModel(topology, nfv_cache_policy, **netconf)
+    model_proposal = NetworkModelHodVnfs(topology, nfv_cache_policy, **netconf)
+    view_baseline = NetworkView(model_baseline)
+    view_proposal = NetworkView(model_proposal)
+    controller_baseline = NetworkController(model_baseline)
+    controller_proposal = NetworkController(model_proposal)
 
-    collectors_inst = [DATA_COLLECTOR[name](view, **params)
+
+    collectors_inst_baseline = [DATA_COLLECTOR[name](view_baseline, **params)
                        for name, params in collectors.items()]
-    collector = CollectorProxy(view, collectors_inst)
-    controller.attach_collector(collector)
+    collector_baseline = CollectorProxy(view_baseline, collectors_inst_baseline)
+    controller_baseline.attach_collector(collector_baseline)
 
-    policy_name = policy['name']
-    policy_args = {k: v for k, v in policy.items() if k != 'name'}
-    policy_inst = POLICY[policy_name](view, controller, **policy_args)
+    policy_name_baseline = policy['GreedyWithoutPlacement']
+    policy_args_baseline = {k: v for k, v in policy.items() if k != 'name'}
+    policy_inst_baseline = POLICY[policy_name_baseline](view_baseline, controller_baseline, **policy_args_baseline)
+
+
+
+    collectors_inst_proposal = [DATA_COLLECTOR[name](view_proposal, **params)
+                                for name, params in collectors.items()]
+    collector_proposal = CollectorProxy(view_proposal, collectors_inst_proposal)
+    controller_proposal.attach_collector(collector_proposal)
+
+    policy_name_proposal = policy['GreedyWithOnlinePlacementPolicy']
+    policy_args_proposal = {k: v for k, v in policy.items() if k != 'name'}
+    policy_inst_proposal = POLICY[policy_name_proposal](view_proposal, controller_proposal, **policy_args_proposal)
+
+
+
 
     for time, event in workload:
-        policy_inst.process_event(time, **event)
-    return collector.results()
+        policy_inst_baseline.process_event(time, **event)
+        policy_inst_proposal.process_event(time, **event)
+    return collector_baseline.results(), collector_proposal.results()
