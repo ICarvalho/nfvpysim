6fbf857b92bdd59889da7ca535f6ada95a8d1d3a
igor <igfuca@gmail.com>
2023-04-09 09:04:20 -0300

    update in policy.py

---
+++ nfvpysim/build/lib/nfvpysim/scenarios/find_top_ranked_rules.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+#file = "hon_sfc_var_len_rules.csv"
+#file = "hon_sfc_by_len_rules.csv"
+file = "hon_random_sfc_rules.csv"
+
+
+#file = "sfc_seq_len_8_rules.csv"
+
+df = pd.read_csv(file,  header=None, delimiter=',', names=list(range(9)))
+print(df.nlargest(20, columns=[4]))
