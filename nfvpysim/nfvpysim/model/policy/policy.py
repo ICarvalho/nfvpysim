2de7b846fc2a5ba6edee1bd30479a0fcfb396e10
igor <igfuca@gmail.com>
2023-04-09 09:26:45 -0300

    update in policy.py

--- nfvpysim/nfvpysim/model/policy/policy.py
+++ nfvpysim/nfvpysim/model/policy/policy.py
@@ -452,10 +452,11 @@ class Hod(Policy):
                         self.controller.put_vnf(v, missed_vnf)
                         self.controller.vnf_proc(missed_vnf)
                         self.controller.proc_vnf_payload(u, v)
-                if all(value == 1 for value in vnf_status.values()) and delay_sfc[sfc_id] <= delay:
-                    delay_sfc[sfc_id] += sum_cpu_sfc
-                    self.controller.sfc_hit(sfc_id)
-                    break
+                if delay_sfc[sfc_id] is not None:
+                    if all(value == 1 for value in vnf_status.values()) and delay_sfc[sfc_id] <= delay:
+                        delay_sfc[sfc_id] += sum_cpu_sfc
+                        self.controller.sfc_hit(sfc_id)
+                        break
 
         self.controller.end_session()
 
